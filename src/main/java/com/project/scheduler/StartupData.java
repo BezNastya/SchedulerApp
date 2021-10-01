921b131609cbda62ec59e7b9cc8ec255bdaa1eab
Artur Onopriichuk <artur.onopriichuk@ukma.edu.ua>
2021-10-01 23:07:08 +0300

    Teacher entity, TeacherService and StartupData updated. TeacherRepository added.

--- src/main/java/com/project/scheduler/StartupData.java
+++ src/main/java/com/project/scheduler/StartupData.java
@@ -2,8 +2,10 @@ package com.project.scheduler;
 
 import com.project.scheduler.entity.Admin;
 import com.project.scheduler.entity.Student;
+import com.project.scheduler.entity.Teacher;
 import com.project.scheduler.repository.MasterRepository;
 import com.project.scheduler.repository.StudentRepository;
+import com.project.scheduler.repository.TeacherRepository;
 import com.project.scheduler.service.AdminService;
 import com.project.scheduler.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +16,15 @@ import org.springframework.stereotype.Component;
 public class StartupData implements CommandLineRunner {
 
     private StudentRepository studentRepository;
+    private TeacherRepository teacherRepository;
     private MasterRepository masterRepository;
 
     @Autowired
-    public StartupData(StudentRepository studentRepository, MasterRepository masterRepository) {
+    public StartupData(StudentRepository studentRepository,
+                       TeacherRepository teacherRepository,
+                       MasterRepository masterRepository) {
     this.studentRepository = studentRepository;
+    this.teacherRepository = teacherRepository;
     this.masterRepository = masterRepository;
     }
 
@@ -26,6 +32,7 @@ public class StartupData implements CommandLineRunner {
     public void run(String... args) {
         adminAccount();
         studentAccount();
+        teacherAccount();
     }
 
     private void studentAccount(){
@@ -36,13 +43,19 @@ public class StartupData implements CommandLineRunner {
         studentRepository.save(student);
 
     }
+    private void teacherAccount(){
+        Teacher teacher = new Teacher();
+
+        teacher.setEmail("teacher@ukma.edu.ua");
+        teacher.setPassword("teacher");
+        teacherRepository.save(teacher);
+
+    }
     private void adminAccount(){
         Admin admin = new Admin();
 
         admin.setEmail("admin@ukma.edu.ua");
         admin.setPassword("admin");
-
-        //TODO  masterRepository and uncomment function below
-         masterRepository.save(admin);
+        masterRepository.save(admin);
     }
 }
