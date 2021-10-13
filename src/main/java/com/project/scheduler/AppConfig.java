05742bad8b1a748e5dbb61333966a1545700a772
Anastasiia Bezruka <nastyabezruka@gmail.com>
2021-10-13 19:37:43 +0300

    Fixed profiles for dev and prod. Connect h2 database

--- src/main/java/com/project/scheduler/AppConfig.java
+++ src/main/java/com/project/scheduler/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 public class AppConfig {
-    @Value("${spring.profiles.default}")
+    @Value("${spring.profiles.active}")
     private String profile;
 
     @Bean
