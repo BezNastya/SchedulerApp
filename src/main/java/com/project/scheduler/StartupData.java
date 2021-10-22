c1a62d396b3160e07eb1347d6a7300e0a837ef06
Denys <denys.tsabut@ukma.edu.ua>
2021-10-22 13:18:20 +0300

    fix project 1

--- src/main/java/com/project/scheduler/StartupData.java
+++ src/main/java/com/project/scheduler/StartupData.java
@@ -10,7 +10,7 @@ import com.project.scheduler.service.impl.StudentServiceImpl;
 import com.project.scheduler.service.impl.TeacherServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
+//import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
