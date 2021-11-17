0177caac25262e2cbd427647075da0d76d5494e7
Denys <denys.tsabut@ukma.edu.ua>
2021-11-17 09:25:24 +0200

    added WebSecurityConfig, updated User service, rep, added UserDetailsServiceImpl

--- src/main/java/com/project/scheduler/repository/UserRepository.java
+++ src/main/java/com/project/scheduler/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.project.scheduler.repository;
 
 import com.project.scheduler.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(@Param(value = "userEmail") final String userEmail);
+
 }
