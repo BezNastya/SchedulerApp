bb7f07b565752ab70719bf3160cc558613ca8926
YevheniiOsta <yevheniiostashko@gmail.com>
2021-11-30 18:23:56 +0200

    added TeacherWebMvcTest

--- src/test/java/com/project/scheduler/TeacherJPATest.java
+++ src/test/java/com/project/scheduler/TeacherJPATest.java
@@ -1,22 +1,66 @@
 package com.project.scheduler;
 
+import com.project.scheduler.entity.Admin;
 import com.project.scheduler.entity.Teacher;
+import com.project.scheduler.repository.AdminRepository;
 import com.project.scheduler.repository.TeacherRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
-@SpringBootTest
-
+@DataJpaTest
 public class TeacherJPATest {
+
     @Autowired
-    TeacherRepository repository;
+    TeacherRepository teacherRepository;
+
+    @Test
+    void shouldReturnTrueWhenRepoIsNotEmpty() {
+        List<Teacher> teachers = teacherRepository.findAll();
+        assertNotNull(teachers);
+    }
+
+    @Test
+    void shouldReturnTrueWhenRepoIsEmpty() {
+        List<Teacher> teachers = any();
+        assertNull(teachers);
+    }
+
 
     @Test
     public void teachersTest() {
-        Iterable<Teacher> teachers = repository.findAll();
-        assertThat(teachers).hasSize(2);
+        Teacher teacher = new Teacher();
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        teacher.setEmail("teacher@ukma.edu.ua");
+        teacher.setPassword(encoder.encode("teacher"));
+        teacher.setAcademicDegree("Master");
+        teacher.setFirstName("Alina");
+        teacher.setLastName("Petrivna");
+        teacher.setDepartment("FI");
+        teacher.setRole("TEACHER");
+        teacher.setAuthorized(true);
+        teacherRepository.save(teacher);
+        Iterable<Teacher> teachers = teacherRepository.findAll();
+        assertThat(teachers).hasSize(1);
+    }
+
+    @Test
+    void shouldReturnTrueIfNameIsAlina(){
+        Teacher teacher = new Teacher();
+        teacher.setFirstName("Alina");
+        assertEquals(teacher.getFirstName(),"Alina");
     }
 }
+
+
